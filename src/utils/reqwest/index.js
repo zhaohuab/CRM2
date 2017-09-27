98f00691b9a5f9cd4cfd7e81811acaa2f1955f4f
litcb <litcb@yonyou.com>
2017-09-27 18:24:56 +0800

    ok

--- src/utils/reqwest/index.js
+++ src/utils/reqwest/index.js
@@ -5,7 +5,9 @@
  */
 import request from 'reqwest'
 import handle from './HandleReqwest'
-
+import des from 'des.js'
+import crypto from 'crypto';
+import {Buffer} from 'buffer'
 request.ajaxSetup({
     headers: {
         // 后端约定, 用以辨别是否为ajax请求的字段
@@ -16,6 +18,30 @@ request.ajaxSetup({
 
 // session过期自动跳转登录页面
 const reqwest = (options,success,fail)=> {
+    let {data,...others} = options;
+
+    var CBC = des.CBC.instantiate(des.DES);
+    let v = "3132333435363738";
+    var key = new Buffer(v, 'hex');
+    var iv = new Buffer(v, 'hex');
+    var input = new Buffer(v, 'hex');
+    //debugger
+    var enc = CBC.create({  
+        type: 'encrypt',
+        key: key,
+        iv: iv
+    });
+    console.info(key);
+    // console.info(enc.update(input));
+    // console.info(enc.final())
+    var out = enc.update(input).concat(enc.final());
+    
+    //  var cipher = crypto.createCipheriv('des', key, iv);
+    // console.info(cipher.update(input));
+    // console.info(cipher.final());
+    console.info(out);
+
+
     request({
         type:"application/x-www-form-urlencoded",
         ...options
