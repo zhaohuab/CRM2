24044481e4e8f7d4ed8367d3e4b4c9d0b9ae8a52
litcb <litcb@yonyou.com>
2017-11-16 09:30:32 +0800

    文件修改

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,9 @@ var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 //修改antd.design主题文件
 var theme = require("./theme.config.js");
 
-var hostIP = "10.11.112.80";
+var hostIP = "10.11.112.44";
 var portNumber = "3000";
-let urlPath = "10.11.112.80";
+let urlPath = "10.11.112.44";
 
 module.exports = {
     entry: {
