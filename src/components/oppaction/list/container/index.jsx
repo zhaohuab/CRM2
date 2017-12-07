508dcab3393bfaf7af71c0fb9016a3fa7515f5ff
wutongh <wutongh@yonyou.com>
2017-12-07 14:57:19 +0800

    提交

--- src/components/oppaction/list/container/index.jsx
+++ src/components/oppaction/list/container/index.jsx
@@ -103,6 +103,7 @@ class List extends React.Component {
     form.validateFieldsAndScroll((err, values) => {
       if (!err) {
         console.log('Received values of form: ', values);
+        return;
       }
     });
     if (this.state.isEdit) {
