f06b331a11ef2d442f188becf05311df789d65ea
gaoshank <gaoshank@yonyou.com>
2017-12-22 09:14:20 +0800

    退出bug修改，分配表单bug修改

--- src/components/customer/list/container/relevantObject.jsx
+++ src/components/customer/list/container/relevantObject.jsx
@@ -17,6 +17,11 @@ const Panel = Collapse.Panel;
 
 
 export default class RelevantObject extends React.Component {
+    aa(e){
+       
+        e.stopPropagation()
+
+    }
     headerFn(obj){
         let  icon = ['icon-canyuren','icon-lianxirenguanxi','icon-xiansuofenpei','icon-shangji','icon-wenjian']
         let fn = ['']
@@ -25,7 +30,7 @@ export default class RelevantObject extends React.Component {
                 <Col className='left'><i className={'iconfont '+icon[obj.index]} /><span>{obj.title}</span></Col>
                 <Col className='right'>
                     {
-                        obj.newBtn == 'add'?<i className={'iconfont '+icon[obj.index]} />:''
+                        obj.newBtn == 'add'?<i className={'iconfont icon-tianjia'} onClick={this.aa.bind(this)}/>:''
                     }
                 </Col>
             </Row>
@@ -37,7 +42,20 @@ export default class RelevantObject extends React.Component {
             <div className='relevant-wapper' id='relevant-wapper-item'>
                 <Collapse defaultActiveKey={['1']}>
                     <Panel header={this.headerFn({title:'参与人',index:0,newBtn:'add'})} key="1">
-                    1
+                        <div className='join-warpper'>
+                            <p className='join-warpper-item'> 
+                                <div>
+                                    <img
+                                        src={require("assets/images/header/photo.png")}
+                                        className="join-image"
+                                    />
+                                </div>
+                                <div>
+                                    最多五个字
+                                </div>
+                                
+                            </p>
+                        </div>
                     </Panel>
                     <Panel header={this.headerFn({title:'联系人',index:1,newBtn:'add'})} key="2">
                     2
